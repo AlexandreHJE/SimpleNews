e38e86d331b4d989672e0788b64706ddcd2d6bd7
AlexHu <b99b01016@gmail.com>
2021-09-19 23:02:43 +0800

    Initial Commit

---
+++ SimpleNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleNews
+//
+//  Created by Alex Hu on 2021/9/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
